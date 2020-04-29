b16e96b8030dfa10ba066f7e27a7c19f3da4eac2
Harbison Apps <harbisonapps@gmail.com>
2020-04-29 10:56:41 -0700

    .

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,45 +1,44 @@
 <template>
-   <div id="contact" class="mx-auto text-center">
-     <h1>Contact</h1>
-      <h3>Fill this out and I will get back to you as soon as possible</h3>
-      <v-form class="column pt-3 px-6 mx-auto justify-center contact-form" name="contact" data-netlify="true"
-        data-netlify-honeypot="bot-field" action="/thankyou" method="POST"
-        @submit.prevent="submitForm"
-        ref="form"
-        v-model="valid"
-        lazy-validation
-      >
-        <input type="hidden" name="bot-field" />
-        <v-text-field name="name" v-model="form.name" ref="name"
-          :rules="nameRules" autofocus aria-placeholder="Your name is required"
-          label="Name" required
-        ></v-text-field>
-        <v-text-field name="email" v-model="form.email"
-          :rules="emailRules" ref="email" aria-placeholder=""
-          label="E-mail" required
-        ></v-text-field>
-        <v-text-field name="phone" v-model="form.phone"
-          :rules="phoneRules" ref="phone"
-          label="Phone"
-        ></v-text-field>
-        <v-select name="subject" class="body-1" v-model="form.request" :items="form.items" ref="request"
-          :rules="[v => !!v || 'A subject is required']"
-          label='Please contact me' required aria-placeholder="A subject is required"
-          ></v-select>
-        <v-textarea name="message" v-model="form.comment"
-          label="Message" rows="3"
-        ></v-textarea>
-        <!-- <v-btn class="mr-4" outlined color="error" @click="sheet = !sheet"
-          >close
-        </v-btn> -->
-        <v-btn outlined color="warning" class="mr-4" @click="reset"
-          >reset
-        </v-btn>
-        <v-btn color="success" type="submit"
-          > Send
-        </v-btn>
-      </v-form>
-    <!-- </div> -->
+  <div id="contact" class="mx-auto text-center">
+    <h1>Contact</h1>
+    <h3>Fill this out and I will get back to you as soon as possible</h3>
+    <v-form class="column pt-3 px-6 mx-auto justify-center contact-form" name="contact" data-netlify="true"
+      data-netlify-honeypot="bot-field" method="POST"
+      @submit.prevent="submitForm"
+      ref="form"
+      v-model="valid"
+      lazy-validation
+    >
+      <input type="hidden" name="bot-field" />
+      <v-text-field name="name" v-model="form.name" ref="name"
+        :rules="nameRules" autofocus aria-placeholder="Your name is required"
+        label="Name" required
+      ></v-text-field>
+      <v-text-field name="email" v-model="form.email"
+        :rules="emailRules" ref="email" aria-placeholder=""
+        label="E-mail" required
+      ></v-text-field>
+      <v-text-field name="phone" v-model="form.phone"
+        :rules="phoneRules" ref="phone"
+        label="Phone"
+      ></v-text-field>
+      <v-select name="subject" class="body-1" v-model="form.request" :items="form.items" ref="request"
+        :rules="[v => !!v || 'A subject is required']"
+        label='Please contact me' required aria-placeholder="A subject is required"
+        ></v-select>
+      <v-textarea name="message" v-model="form.comment"
+        label="Message" rows="3"
+      ></v-textarea>
+      <!-- <v-btn class="mr-4" outlined color="error" @click="sheet = !sheet"
+        >close
+      </v-btn> -->
+      <v-btn outlined color="warning" class="mr-4" @click="reset"
+        >reset
+      </v-btn>
+      <v-btn color="success" type="submit"
+        > Send
+      </v-btn>
+    </v-form>
   </div>
 </template>
 
--- src/views/SubmitSuccess.vue
+++ src/views/SubmitSuccess.vue
@@ -4,9 +4,7 @@
       <q-icon></q-icon>
     </h1>
     <h1>You're Awesome! Thank you for your submission!</h1>
-    <router-link to="/">
-      <v-btn x-large color="success" @click="$router.go(-2)">Back</v-btn>
-    </router-link>
+    <v-btn x-large color="success" @click="$router.go(-2)">Back</v-btn>
   </div>
 </template>
 
